e96c8480f29198b805a35ba3844a36193d6ebebf
sgunturu <sgunturu@MNI67N69C3.modeln.com>
2021-10-09 13:07:55 +0530

    war file

--- src/main/java/com/epidemic/EpidemicApplication.java
+++ src/main/java/com/epidemic/EpidemicApplication.java
@@ -2,6 +2,8 @@ package com.epidemic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
@@ -9,7 +11,12 @@ import com.epidemic.*;
 
 @SpringBootApplication
 @ComponentScan(basePackages ={ "com.epidemic, com.epidemic.controller, com.epidemic.models, com.epidemic.repositories, com.epidemic.services ,com.example.demo"} )
-public class EpidemicApplication {
+public class EpidemicApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EpidemicApplication.class);
+	}
 
 // new  branch
 	public static void main(String[] args) {
